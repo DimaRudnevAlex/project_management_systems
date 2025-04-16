1a7926e93c842b173337acf8860246ac0b0694ae
RudnevDima <drud20000@mail.ru>
2025-04-17 00:52:06 +0300

    Create page of issues - Create filter component - Create list issues component - Add responsive

---
+++ src/@types/uikit/index.ts
@@ -0,0 +1,3 @@
+export interface IUiSelect {
+    text: string
+}
---
+++ src/components/uikit/TextField/index.tsx
@@ -0,0 +1,7 @@
+import { TextField } from '@mui/material'
+
+const UiTextField = () => {
+    return <TextField label="Поиск" variant="filled" color="secondary" />
+}
+
+export default UiTextField
